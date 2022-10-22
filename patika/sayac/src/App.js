8072f11f2f293569c453e21f9efb6a3125cdfcea
Emree17 <82042889+Emree17@users.noreply.github.com>
2022-10-22 15:24:47 +0300

    Counter App

---
+++ patika/sayac/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Counter from './components/Counter';
+import './components/Counter.css';
+
+
+function App() {
+  return (
+    <div >
+     <Counter/>
+    </div>
+  );
+}
+
+export default App;
